ef6ea8697146ab9eb08720b54e0f58f2bdf0a613
Sarah Sicard <ssicard@meroxa.io>
2025-03-05 09:44:16 -0700

    update default conduit version

--- api/v1alpha/conduit_types.go
+++ api/v1alpha/conduit_types.go
@@ -41,7 +41,7 @@ var ConduitConditions = NewConditionSet(
 )
 
 const (
-	ConduitVersion             = "v0.11.1"
+	ConduitVersion             = "v0.13.2"
 	ConduitImage               = "ghcr.io/conduitio/conduit"
 	ConduitContainerName       = "conduit-server"
 	ConduitPipelinePath        = "/conduit.pipelines"
@@ -57,7 +57,7 @@ const (
 
 var (
 	ConduitPipelineFile      = path.Join(ConduitPipelinePath, "pipeline.yaml")
-	ConduitEarliestAvailable = "v0.11.1"
+	ConduitEarliestAvailable = "v0.13.2"
 )
 
 // ConduitSpec defines the desired state of Conduit
